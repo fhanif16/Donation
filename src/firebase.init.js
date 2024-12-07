d4876ff96ab689737ae586178251a2508e3360c5
fhanif16 <fatemahanif16@gmail.com>
2024-12-07 18:32:32 -0500

    env added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,19 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBq9bt1rTzNWU_ciewiRY2hYGMCwkVBpjQ",
-  authDomain: "heart-lift.firebaseapp.com",
-  projectId: "heart-lift",
-  storageBucket: "heart-lift.firebasestorage.app",
-  messagingSenderId: "609874140981",
-  appId: "1:609874140981:web:d7fc27d58e5acb7a3e767c"
+  // apiKey: "AIzaSyBq9bt1rTzNWU_ciewiRY2hYGMCwkVBpjQ",
+  // authDomain: "heart-lift.firebaseapp.com",
+  // projectId: "heart-lift",
+  // storageBucket: "heart-lift.firebasestorage.app",
+  // messagingSenderId: "609874140981",
+  // appId: "1:609874140981:web:d7fc27d58e5acb7a3e767c"
+
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
